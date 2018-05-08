2691f799e791efbb6e817df4fcd4ae59bcc77f25
Price Comstock <price.comstock@gmail.com>
2018-05-07 21:35:16 -0500

    copied from another vue app

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class="section">
+      <lesson-list></lesson-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import lessonList from './LessonList.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  components: {
+    lessonList
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/LessonItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="box">
+    <div class="field is-grouped">
+      <p class="control timeinput has-icons-left">
+        <input @input="updateTime($event.target.value)" class="input" type="number" placeholder="mm">
+        <span class="icon is-small is-left">
+          <i class="fas fa-clock"></i>
+        </span>
+      </p>
+      <p class="control is-expanded has-icons-left">
+        <input @input="updateName($event.target.value)" class="input" type="text" placeholder="name">
+        <span class="icon is-small is-left">
+          <i class="fas fa-lightbulb"></i>
+        </span>
+      </p>
+      <button class="delete" @click="$emit('delete')"></button>
+      <span class="icon" @click="$emit('moveup')">
+        <i class="fas fa-arrow-alt-circle-up"></i>
+      </span>
+      <span class="icon" @click="$emit('movedown')">
+        <i class="fas fa-arrow-alt-circle-down"></i>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'lessonItem',
+  data () {
+    return {
+    }
+  },
+  props: {
+      name: {
+          type: String,
+          required: false
+      },
+      time: {
+          type: String,
+          required: false
+      }
+  },
+  methods: {
+    updateTime(newTime) {
+      this.$emit('update:time', newTime)
+    },
+    updateName(newName) {
+      this.$emit('update:name', newName)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.timeinput {
+  width:90px;
+}
+</style>
