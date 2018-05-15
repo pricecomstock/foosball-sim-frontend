f28dcad11f7836b4135e1ce9d8b42a3bde5db240
Price Comstock <price.comstock@gmail.com>
2018-05-14 21:44:33 -0500

    small updates

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,38 +1,53 @@
 <template>
   <div class="box">
+    <p class="is-size-7">
+      <strong>Game #{{ game.gameNumber }}</strong>
+    </p>
     <nav class="level is-mobile">
-      <div class="level-left">
+      <div class="level-item">
         <div class="level-item has-text-centered score">
           <div>
-            <p class="heading">{{ game.winner.player }}</p>
-            <p class="title">{{ game.winner.score }}</p>
-            <p>
+            <p class="heading">{{ game.winner.player.name }} [+{{ game.eloChange.toFixed(1) }}]</p>
+            <p class="title tag is-success is-large">{{ game.winner.score }}</p>
+            <!-- <p>
               <span class="icon is-small">
                 <i v-for="i in game.winner.score" class="fas fa-futbol pointicon" :key="i"></i>
               </span>
-            </p>
+            </p> -->
           </div>
         </div>
       </div>
-      <div class="level-item has-text-centered">
+      <!-- <div class="level-item has-text-centered">
         <div>
           <p class="heading">Elo</p>
-          <p class="subtitle">{{ game.eloChange.toFixed(1) }}</p>
+          <p class="subtitle">
+            <span class="icon is-small">
+              <i class="fa fa-angle-left"></i>
+            </span>
+            {{ game.eloChange.toFixed(1) }}
+          </p>
         </div>
-      </div>
-      <div class="level-right">
+      </div> -->
+      <div class="level-item">
         <div class="level-item has-text-centered score">
           <div>
-            <p class="heading">{{ game.loser.player }}</p>
-            <p class="title">{{ game.loser.score }}</p>
-            <p>
+            <p class="heading">{{ game.loser.player.name }} [-{{ game.eloChange.toFixed(1) }}]</p>
+            <p class="title tag is-white is-large">{{ game.loser.score }}</p>
+            <!-- <p>
               <span class="icon is-small">
                 <i v-for="i in game.loser.score" class="fas fa-futbol pointicon" :key="i"></i>
               </span>
-            </p>
+            </p> -->
           </div>
         </div>
       </div>
+      <!-- <div class="level-right has-text-centered expand-arrow" @click="expanded = !expanded">
+        <div class="level-item">
+          <span class="icon">
+            <i class="fas fa-lg fa-angle-down"></i>
+          </span>
+        </div>
+      </div> -->
     </nav>
   </div>
 </template>
@@ -44,17 +59,26 @@ export default {
       game: {
           required: true
       }
+  },
+  data () {
+    return {
+      expanded: false
+    }
   }
 }
 </script>
 
 <style>
 .score {
-  min-width: 3rem;
+  min-width: 5rem;
 }
 
 .pointicon {
   font-size: 0.6em;
   overflow-wrap: break-word;
+}
+
+.expand-arrow {
+  cursor: pointer;
 }
 </style>
--- src/components/LeagueDashboard.vue
+++ src/components/LeagueDashboard.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="columns">
-      <div class="column is-9">
-        <elo-chart v-if="eloLoaded" :players="players" :elos="elos" :height="600" :width="1000"></elo-chart>
+      <div class="column is-9-desktop is-10-fullhd">
+        <!-- <div class="tabs">
+          <ul>
+            <li :class="{isActive: eloChart}"><a>Elo</a></li>
+            <li><a>Points Scored</a></li>
+            <li><a>King</a></li>
+            <li><a>Special Events</a></li>
+            <li><a>Game Stats</a></li>
+          </ul>
+        </div> -->
+        <component :is="elo-chart" :players="players" :elos="elos" :height="600" :width="1000"></component>
+        <elo-chart v-if="eloLoaded" :players="players" :elos="elos" :height="600" :width="1000" ></elo-chart>
       </div>
-      <div class="column is-3">
+      <div class="column is-3-desktop is-2-fullhd">
         <game-feed :game-list="gameHistory"></game-feed>
       </div>
     </div>
@@ -32,6 +42,7 @@ export default {
   created() {
     axios.get(`/elos/${this.name}`)
       .then( res => {
+          console.log('elos', res)
           this.players = res.data.players
           this.elos = res.data.elos
           this.$nextTick( () => {
@@ -42,8 +53,13 @@ export default {
     
     axios.get(`/gamehistory/${this.name}`)
       .then( res => {
-          console.log(res)
+          console.log('gamehistory', res)
           this.gameHistory = res.data.games
+          this.gameHistory = this.gameHistory.map( (game, index) => {
+            game.gameNumber = index + 1
+            return game
+          })
+          this.gameHistory.reverse()
       })
       .catch(error => console.log(error))
   }
